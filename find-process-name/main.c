b943ce76534fafb16c6de06707808f1069e6491a
Marshall Schmutz <darthmars97@gmail.com>
2024-03-20 09:03:19 -0600

    changing process name with C kind of works now

--- find-process-name/main.c
+++ find-process-name/main.c
@@ -1,38 +1,41 @@
-#include <sys/sysctl.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void set_proc_name(const char* name) {
-    int mib[4];
-    size_t len;
-    struct kinfo_proc kp;
-
-    pid_t pid = getpid();
-
-    mib[0] = CTL_KERN;
-    mib[1] = KERN_PROC;
-    mib[2] = KERN_PROC_PID;
-    mib[3] = pid;
-
-    len = sizeof(kp);
-    if (sysctl(mib, 4, &kp, &len, NULL, 0) == -2) {
-        perror("sysctl");
-        return;
+void gen_string(int seed, char* output, int length) {
+    for (int i = 0; i < length; ++i) {
+        output[i] = (char) ((seed + i) % 0xFFFF + 1);
     }
-
-    strncpy(kp.kp_proc.p_comm, name, sizeof(kp.kp_proc.p_comm));
-    kp.kp_proc.p_comm[sizeof(kp.kp_proc.p_comm) - 1] = '\0';
-
-    if (sysctl(mib, 4, NULL, NULL, &kp, len) == -1) {
-        perror("sysctl");
-        return;
-    }
+    output[length] = '\0';
 }
 
-int main() {
-    set_proc_name("mershy");
-    printf("Press enter to continue...");
+int main(int argc, char* argv[]) {
+    int seed = 42;
+    int length = 7;
+    char* output = (char*) malloc(length + 1);
+
+    if (output == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+
+    gen_string(seed, output, length);
+    printf("output: %s\n", output);
+
+    for (int i = 0; i < length; ++i) {
+        printf("%d ", output[i]);
+    }
+    printf("\n");
+
+    printf("pid: %i\n", getpid());
+    char psname[] = "mershy";
+    strncpy(argv[0], psname, strlen(argv[0]));
+
+    printf("Press Enter to continue...");
     getchar();
+
+    free(output);
+
     return 0;
 }
